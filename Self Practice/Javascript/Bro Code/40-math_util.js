8678cde5142faa9b7eef6782e21741a5fd6403ec
Monye Jude <Judechuks26@gmail.com>
2023-04-02 14:57:58 +0100

    some math module

---
+++ Self Practice/Javascript/Bro Code/40-math_util.js
@@ -0,0 +1,10 @@
+// To export variables and functions proceed with them with the keyword 'export'
+export const PI = 3.14159;
+
+export function myCircumference(radius){
+  return 2 * PI * radius;
+}
+
+export function myArea(radius){
+  return PI * radius * radius;
+}
