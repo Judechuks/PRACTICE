fb0dec35c20582b380a054301b6857723af71eda
Monye Jude <Judechuks26@gmail.com>
2023-04-06 16:51:53 +0100

    43-DOM-example.js updated

--- Self Practice/Javascript/Bro Code/43-DOM-example.js
+++ Self Practice/Javascript/Bro Code/43-DOM-example.js
@@ -17,3 +17,17 @@ vegetables[2].style.backgroundColor = "grey";
 let desserts = document.getElementsByClassName("desserts");
 desserts[0].style.backgroundColor = "purple";
 desserts[1].style.backgroundColor = "crimson";
+
+let element = document.querySelector("#myTitle");
+element.style.backgroundColor = "lightgreen";
+
+let ul = document.querySelector("ul");
+ul.style.backgroundColor = "lightgrey";
+
+let label = document.querySelector("[for]"); // selects the first label with attribute of 'for'
+// let label = document.querySelector("[for=name]"); //label with attribute of 'for' value of 'name'
+label.style.color = "green";
+label.style.fontWeight = "bold";
+
+// let labels = document.querySelectorAll("[for"); 
+// select all the labels with attribute of 'for' and forms an array with it.
