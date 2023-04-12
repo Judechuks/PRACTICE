62bc08658bb709473fa7b38c4ad8ee52e5c94c52
Monye Jude <Judechuks26@gmail.com>
2023-04-12 06:50:41 +0100

    48-key-event.js

---
+++ Self Practice/Javascript/Bro Code/48-key-event.js
@@ -0,0 +1,3 @@
+// Keydown = an event, when a user press down a key on the keyboard.
+// Keyup = an event, when a user releases a key on the keyboard, after a keydown event.
+window.addEventListener("keydown", event => console.log(event.key));
