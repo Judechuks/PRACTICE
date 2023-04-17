f4d723ab036f784d38ff4bafcd47ba9bab81c0eb
Monye Jude <Judechuks26@gmail.com>
2023-04-17 14:56:57 +0100

    40-module.js modified

--- Self Practice/Javascript/Bro Code/40-module.js
+++ Self Practice/Javascript/Bro Code/40-module.js
@@ -1,11 +1,11 @@
 // The idea behind a module is that it's a file of reusable code
-// We can import sections of pre-written code to use whenever we want 
+// We can import sections of pre-writen code to use whenever we want 
 // Great for any general utility values and functions
-// Helps to make your code more reusable and maintainable
+// Helps to make your code more reusable and mantainable
 // Think of modules as separate chapters of a book
-// Working with modules, in your html file, in the script tag, add an attribute (type="module")
+// Working with modules, in your html file, in the script ag, add an attribute (type="module")
 
-// use 'import', 'from' keyword and absolute or relative file path to import the variables and functions
+// use 'import', from' keyword and absolute or relative file path to import the variables and functions
 import {PI, myCircumference, myArea} from "./40-math_util.js";
 
 let circumference = myCircumference(10);
