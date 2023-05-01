0cbbab90ca026dd54a278b7d5c1e835ebfe47f46
Monye Jude <Judechuks26@gmail.com>
2023-05-01 20:40:19 +0100

    04-array-methods.js

--- Self Practice/Javascript/Dave Gray/04-array-methods.js
+++ Self Practice/Javascript/Dave Gray/04-array-methods.js
@@ -25,4 +25,12 @@ const array2 = ['d', 'e', 'f'];
 const array3 = array1.concat(array2);
 console.log(array3); // Expected output: Array ["a", "b", "c", "d", "e", "f"]
 
-// 4. 
+// 4. copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.
+// 4a. copyWithin(target). target = zero-based index at which to copy the sequence to.(<-ve count backward)
+console.log([1, 2, 3, 4, 5].copyWithin(2));  // [1, 2, 3, 1, 2]
+console.log([1, 2, 3, 4, 5].copyWithin(-2));  // [1, 2, 3, 1, 2]
+// 4b. copyWithin(target, start). start = zero-based index at which to start copying elements from. (<-ve count backward)
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3));  // [4, 5, 3, 4, 5]
+// 4c. copyWithin(target, start, end). end = zero-based index at which to end copying elements from. (<-ve count backward)
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3, 4));  // [4, 2, 3, 4, 5]
+console.log([1, 2, 3, 4, 5].copyWithin(-2, -3, -1));  // [1, 2, 3, 3, 4]
