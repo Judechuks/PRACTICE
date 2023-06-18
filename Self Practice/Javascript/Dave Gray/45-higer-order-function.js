451c91ab0634d3d1c06739a7e4e3f47ea68781b7
Monye Jude <Judechuks26@gmail.com>
2023-06-18 15:27:51 +0100

    45-higer-order-function.js

---
+++ Self Practice/Javascript/Dave Gray/45-higer-order-function.js
@@ -0,0 +1,13 @@
+// Higher Order Function is a function that does at least one of the following:
+// i. Takes one or more functions as an argument (parameter)
+// ii. Returns a function as its result 
+// Examples :
+// forEach()
+// filter()
+// map()
+// reduce()
+
+// See the array and string method treated before
+array.forEach(element => {
+  console.log(element);
+});
