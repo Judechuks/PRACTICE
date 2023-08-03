76399205399e1493a3e81b82b4785be51f2661bb
Monye Jude <Judechuks26@gmail.com>
2023-08-03 07:56:08 +0100

    main.js

--- Self Practice/TailwindCSS/Lightcode/jayfinance/build/main.js
+++ Self Practice/TailwindCSS/Lightcode/jayfinance/build/main.js
@@ -21,22 +21,21 @@ const userTexts = document.getElementsByClassName('user-text');
 const userPics = document.getElementsByClassName('user-pic');
 
 function showReview() {
-    for(const userPic of userPics) {
-        userPic.classList.remove("active-pic");
-    }
-    for(const userText of userTexts) {
-        userText.classList.remove("active-text");
-    }
+  for(const userPic of userPics) {
+      userPic.classList.remove("active-pic");
+  }
+  for(const userText of userTexts) {
+      userText.classList.remove("active-text");
+  }
 
-    let i = Array.from(userPics).indexOf(event.target);
+  let i = Array.from(userPics).indexOf(event.target);
 
-    userPics[i].classList.add('active-pic');
-    userTexts[i].classList.add('active-text');
-    // 1:11:18
+  userPics[i].classList.add('active-pic');
+  userTexts[i].classList.add('active-text');
+  
 }
 
 const toggleBtn = document.getElementById('toggleBtn');
-
 const card_1_front = document.querySelector("#card_1_front");
 const card_1_back = document.querySelector("#card_1_back");
 
@@ -56,3 +55,36 @@ toggleBtn.addEventListener('change', () => {
   card_3_front.classList.toggle('-rotate-y-180');
   card_3_back.classList.toggle('rotate-y-180');
 });
+
+// SCROLL
+const navbar = document.querySelector('header');
+const sectionn = document.querySelectorAll('section');
+const navlinkss = document.querySelectorAll('nav ul li a');
+
+window.onscroll = () => {
+  if(window.scrollY > 100) {
+    navbar.classList.add('bg-color-primary-dark');
+    navbar.classList.add('border-b');
+    navbar.classList.add('border-color-gray');
+  } else {
+    navbar.classList.remove('bg-color-primary-dark');
+    navbar.classList.remove('border-b');
+    navbar.classList.remove('border-color-gray');
+  }
+
+  // active link
+  let current = 'home';
+
+  sectionn.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if(pageYOffset >= sectionTop-60) {
+          current = section.getAttribute('id');
+      }
+  })
+  navlinkss.forEach((item) => {
+      item.classList.remove('text-color-secondary');
+      if(item.href.includes(current)) {
+          item.classList.add("text-color-secondary");
+      } 
+  });
+}
