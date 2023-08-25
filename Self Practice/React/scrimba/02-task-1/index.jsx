9b417d686d3056c1bef259197eb8fb72fd03e2b3
Jude Chuks <judechuks26@gmail.com>
2023-08-25 20:09:08 +0100

    index.jsx

---
+++ Self Practice/React/scrimba/02-task-1/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ul>
+    <h1>Pet Animals</h1>
+    <li>Dog</li>
+    <li>Cat</li>
+    <li>Parrot</li>
+  </ul>, 
+  document.getElementById("root")
+)
