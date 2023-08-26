2dc233349d58d13b8a8b6a852b5ab517e35a26a7
Jude Chuks <judechuks26@gmail.com>
2023-08-26 20:19:27 +0100

    parent and child component in JSX

---
+++ Self Practice/React/scrimba/06-parent-child-component/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Footer(){
+  return (
+    <footer>
+      <img src="./img/logo.png" alt="logo" />
+      <p>Copyright &copy; 2023; <b>Judechuks.</b></p>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Self Practice/React/scrimba/06-parent-child-component/components/MyInfo.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function MyInfo(){
+    return (
+      <div>
+        <h1>Monye Jude</h1>
+        <p>A frontend developer, currently learning ReactJS</p>
+        <ul>
+          <h2>Vacation Spots</h2>
+          <li>Canada</li>
+          <li>USA</li>
+          <li>China</li>
+        </ul> 
+      </div>
+    )
+  }
+  
+export default MyInfo
