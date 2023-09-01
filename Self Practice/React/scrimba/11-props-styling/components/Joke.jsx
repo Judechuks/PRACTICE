d27c08d72d49eaaeb10d5fa31438f6e40460c037
Jude Chuks <judechuks26@gmail.com>
2023-09-01 09:00:30 +0100

    11-props-styling\

---
+++ Self Practice/React/scrimba/11-props-styling/components/Joke.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Joke(props){
+  return (
+    <div className="">
+      <h3 className="before:content-['\003F'] before:text-2xl" style={{display: props.question ? "block" : "none", color: "#3c3"}}>Question: {props.question}</h3>
+      <h3 className="before:content-['\2714']" style={{color: "#33c"}}>Answer: {props.punchline}</h3>
+    </div>
+  )
+}
+/*
+  You can style an element based on a condition, if that condition is met. as seen above
+  You can also use style={{display: !props.question && "none"}}
+  in place of style={{display: props.question ? "block" : "none"}}
+*/ 
+
+export default Joke
