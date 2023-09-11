574ab9c7de32d1c8a1f214111d0c79c78b80c0be
Jude Chuks <judechuks26@gmail.com>
2023-09-11 16:55:14 +0100

    App.jsx

--- Self Practice/React/PedroTech/06-task-3/App.jsx
+++ Self Practice/React/PedroTech/06-task-3/App.jsx
@@ -1,24 +1,30 @@
 import {useState} from 'react'
 
 function App() {
-  const [count, setState] = useState(0)
+  const [count, setCount] = useState(0)
 
-  const handleChange = () => {
-    // setState(!showText)
+  const increase = () => {
+    setCount(count + 1)
+  }
+  const decrease = () => {
+    setCount(count - 1)
+  }
+  const zero = () => {
+    setCount(0)
   }
 
   return ( 
-    <div className=''>
-      <button onClick={handleChange} className='px-2 py-1 text-xl bg-blue-700 text-white'>
+    <div className='w-fit mx-auto my-32'>
+      <h2 className='text-6xl mb-3 text-center'>{count}</h2>
+      <button onClick={increase} className='px-2 py-1 text-xl bg-green-700 text-white rounded-s-md'>
        Increase
       </button>
-      <button onClick={handleChange} className='px-2 py-1 text-xl bg-blue-700 text-white'>
-       Decrease
-      </button>
-      <button onClick={handleChange} className='px-2 py-1 text-xl bg-blue-700 text-white'>
+      <button onClick={zero} className='px-2 py-1 text-xl bg-red-700 text-white'>
        Set to Zero
       </button>
-      <h2 className='text-3xl'>{count}</h2>
+      <button onClick={decrease} className='px-2 py-1 x-4 text-xl bg-blue-700 text-white rounded-e-md'>
+       Decrease
+      </button>
     </div>
   )
 }
