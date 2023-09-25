0f8c6c00456f94f7c2e937bf260cc1dc43d7ea78
Jude Chuks <judechuks26@gmail.com>
2023-09-25 11:21:39 +0100

    Navbar in React

---
+++ Self Practice/React/integration/Navbar/README.md
@@ -0,0 +1,5 @@
+## React
+You have to set up react and install the following:
+
+### 1. React Icons
+### 2. React Router-dom
---
+++ Self Practice/React/integration/Navbar/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home}/>
+          <Route path='/reports' Component={Reports}/>
+          <Route path='/products' Component={Products}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
