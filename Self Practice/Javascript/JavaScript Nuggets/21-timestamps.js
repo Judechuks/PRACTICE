ecb74a3e0eadb9628c7aab9bbc456c8461dc6e9f
Jude Chuks <judechuks26@gmail.com>
2023-12-04 19:44:11 +0100

    21-timestamps.js

--- Self Practice/Javascript/JavaScript Nuggets/21-timestamps.js
+++ Self Practice/Javascript/JavaScript Nuggets/21-timestamps.js
@@ -14,22 +14,22 @@ console.log(new Date().valueOf());
 
 // create id's in app
 let people = [];
-people = [...people, {id: Date.now(), name: 'john'}];
+people = [...people, { id: Date.now(), name: "john" }];
 
 const generateID = (person) => {
   setTimeout(() => {
-    people = [...people, {id: Date.now(), name: person}];
+    people = [...people, { id: Date.now(), name: person }];
     console.log(people);
-  }, 50)
-}
-generateID('jude');
+  }, 50);
+};
+generateID("jude");
 
-// create/get date 
+// create/get date
 const currentDate = Date.now();
 const futureDate = new Date(currentDate + 1000 * 3600 * 48);
-console.log('Future Date:', new Date(futureDate).toUTCString());
+console.log("Future Date:", new Date(futureDate).toUTCString());
 
 // difference between dates
 const secondDate = new Date(2033, 10, 17);
 countDown = secondDate - currentDate;
-console.log('Count Down:', countDown/3600000, 'hours.');
+console.log("Count Down:", countDown / 3600000, "hours.");
