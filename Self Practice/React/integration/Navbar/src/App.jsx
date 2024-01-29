9bdd70103e1bfecac8ed2e114f9077f4f0eef295
Jude Chuks <judechuks26@gmail.com>
2024-01-29 13:44:30 +0100

    prettier reformatted code

--- Self Practice/React/integration/Navbar/src/App.jsx
+++ Self Practice/React/integration/Navbar/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Reports from './pages/Reports'
-import Products from './pages/Products'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -11,13 +11,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact Component={Home}/>
-          <Route path='/reports' Component={Reports}/>
-          <Route path='/products' Component={Products}/>
+          <Route path="/" exact Component={Home} />
+          <Route path="/reports" Component={Reports} />
+          <Route path="/products" Component={Products} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
