13df54006b5704a850ab9950e5544daca9f92466
Jude Chuks <judechuks26@gmail.com>
2024-03-30 20:58:15 +0100

    added toString() to prevent the digits from disappearing

--- Self Practice/React/reusabe/components/countdown/CountDownTimer.jsx
+++ Self Practice/React/reusabe/components/countdown/CountDownTimer.jsx
@@ -12,10 +12,10 @@ const CountDownTimer = ({ date, time }) => {
 
     if (difference > 0) {
       timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / (1000 * 60)) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)).toString(),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24).toString(),
+        minutes: Math.floor((difference / (1000 * 60)) % 60).toString(),
+        seconds: Math.floor((difference / 1000) % 60).toString(),
       };
     }
     return timeLeft;
