9cd5016404a28ab96472bcce510259e4cb84ca3f
Jude Chuks <judechuks26@gmail.com>
2025-02-08 21:48:57 +0100

    modified files

--- Self Practice/React/scrimba/10-props/App.jsx
+++ Self Practice/React/scrimba/10-props/App.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import ContactCard from "./components/ContactCard"
+import React from "react";
+import ContactCard from "./components/ContactCard";
 
 /*
 function App(){
@@ -35,44 +35,44 @@ function App(){
 Can also be replaced using an object (contact) as shown below:
 */
 
-function App(){
+function App() {
   return (
     <div className="contacts">
       <ContactCard
         contact={{
-        name: "Mr. Evans",
-        imgUrl: "./src/assets/team1.jpg",
-        phone: "+234 814 463 8926",
-        email: "evans@email.com"
+          name: "Mr. Evans",
+          imgUrl: "./src/assets/team1.jpg",
+          phone: "+234 814 463 8926",
+          email: "evans@email.com",
         }}
         /*These details may not be hard coded, rather gottens from JSON file*/
       />
       <ContactCard
         contact={{
-        name: "Mrs. Eve",
-        imgUrl: "./src/assets/team-2.jpg",
-        phone: "+234 814 463 4951",
-        email: "eve@email.com"
+          name: "Mrs. Eve",
+          imgUrl: "./src/assets/team-2.jpg",
+          phone: "+234 814 463 4951",
+          email: "eve@email.com",
         }}
       />
       <ContactCard
         contact={{
-        name: "Mr. Richards",
-        imgUrl: "./src/assets/team-3.jpg",
-        phone: "+234 814 493 4221",
-        email: "richard@email.com"
+          name: "Mr. Richards",
+          imgUrl: "./src/assets/team-3.jpg",
+          phone: "+234 814 493 4221",
+          email: "richard@email.com",
         }}
       />
       <ContactCard
         contact={{
-        name: "Mrs. Martha",
-        imgUrl: "./src/assets/team-4.jpg",
-        phone: "+234 814 243 2234",
-        email: "martha@email.com"
+          name: "Mrs. Martha",
+          imgUrl: "./src/assets/team-4.jpg",
+          phone: "+234 814 243 2234",
+          email: "martha@email.com",
         }}
       />
     </div>
-  )
+  );
 }
-  
-export default App
+
+export default App;
