28a5e447f4c35fd0df4b0de87e3a266a26afcc9a
Jude Chuks <judechuks26@gmail.com>
2025-03-27 10:17:32 +0100

    using @tanstack/react-query - useQuery to fetch data from an api

--- Self Practice/React/PedroTech/17-react-query/pages/Home.jsx
+++ Self Practice/React/PedroTech/17-react-query/pages/Home.jsx
@@ -1,28 +1,38 @@
-import { useQuery } from "@tanstack/react-query"
-import Axios from 'axios'
+import { useQuery } from "@tanstack/react-query";
 
 function Home() {
-  const {data: catData, isLoading, isError, refetch} = useQuery(["cat"], () => {
-    // you can rename the data by using data: catData,
-    return Axios.get('https://catfact.ninja/fact').then((response) => response.data)
-  }) 
-  // ["cat"] is the unique id for the query for future use or caching
-
-  if (isError) {
-    return <h1>Sorry, there was an error</h1>
-  }
+  const {
+    data: catData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ["cat"],
+    queryFn: async () => {
+      return fetch("https://catfact.ninja/fact").then((res) => res.json());
+    },
+  });
+  //The useQuery hook is used to get data from an API
 
   if (isLoading) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
+  }
+
+  if (isError) {
+    return <h1>Error: Couldn't fetch facts.</h1>;
   }
 
   return (
     <main>
-      <h1>THIS IS THE <b className="font-extrabold">HOME</b> PAGE</h1>
-      <h3 className="text-2xl">{catData?.fact}</h3> 
+      <h1>
+        THIS IS THE <b className="font-extrabold">HOME</b> PAGE
+      </h1>
+      <h3 className="text-2xl">{catData?.fact}</h3>
       {/*the data?.fact means it should get the fact only when the data returned value is not null */}
-      <button onClick={refetch} className="btn mt-3">Update Data</button>
+      <button onClick={refetch} className="btn mt-3">
+        Update Data
+      </button>
     </main>
-  )
+  );
 }
-export default Home 
+export default Home;
